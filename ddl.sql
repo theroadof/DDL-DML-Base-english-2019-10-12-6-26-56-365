1877cd4eb0bfffdb780b98f81fbcded15fad5c9b
theroadof <1294911641@qq.com>
2020-06-21 13:36:12 +0800

    commit

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,46 @@
 -- **Database level:**
 -- Display all the database
+show databases;
 
 -- Enter a certain database
+use student;
 
 -- Create a database
+Create database student;
 
 -- Create the database of the designated character set
+Create database student character set utf8mb4;
 
 -- Display the creation information fo the database
+show create database student;
 
 -- Revise the codes of the database
+alter database student character set utf8;
 
 -- Delete a database
+drop database student;
 
 -- **Table level**
 -- Revise table name
+alter table student rename students;
 
 -- Revise the field's data type
+alter table student modify name varchar(30);
 
 -- Revise field name
+alter table student change sex gender varchar(15);
 
 -- Add field
+alter table student add column email varchar(50);
 
 -- Delete field
+alter table student drop column email;
 
 -- Revise the table's storage engine
+alter table student engine = MyISAM;
 
 -- Delete the table's foreign key restriant
+alter table student drop foreign key foreign_key_name;
 
 -- Delete a table
+drop table student;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,12 @@
 -- Insert record
+Insert into student values('001','Colin',18,'male'),('002','Emma',20,'female');
 
 -- Revise record
+update student set age =20 where id = '001';
+update student set age =18 where id = '002';
 
 --  Delete record
+delete from student where id ='001';
 
 -- Search record
+select * from student;
